74dc9d3c5606283e81b6c1f6ee985f86e4ca2230
Amjad Dabi <amjad.dabi@gmail.com>
2024-04-09 11:06:22 -0400

    Added RMSE calculations and sorting of mutations

--- scripts/create_divergence_plots.py
+++ scripts/create_divergence_plots.py
@@ -21,6 +21,21 @@ stat = args.stat
 muts = args.muts
 mut_labels = args.mut_labels
 
+# Sort muts and mut_labels so that the order is always Neutral -> Beneficial -> Deleterious
+def sort_key(x):
+    if x == 'Neutral':
+        return 0
+    elif x == 'Beneficial':
+        return 1
+    elif x == 'Deleterious':
+        return 2
+    else:
+        return 3
+
+if muts is not None and mut_labels is not None:
+    muts, mut_labels = zip(*sorted(zip(muts, mut_labels), key=lambda x: sort_key(x[1])))
+
+
 if stat != 'ld':
     if muts is None or mut_labels is None:
         argparser.error('Must provide mutation types and labels if plot statistic is not ld')
@@ -29,8 +44,11 @@ divergence_type = args.type
 input_file = Path(args.input)
 output_dir = Path(args.output)
 
+mut_colors = ListedColormap(plt.cm.viridis(np.linspace(0, 1, 3)))
+mut_colors_dict = {'Neutral': mut_colors(0), 'Beneficial': mut_colors(1), 'Deleterious': mut_colors(2)}
 
-if divergence_type not in ['kl', 'mean_percent_error', 'median_percent_error']:
+
+if divergence_type not in ['kl', 'mean_percent_error', 'median_percent_error', 'average_rmse']:
     raise ValueError('Divergence type must be one of "kl" for KL divergence or "mean_percent_error" for mean percent error')
 
 data = pd.read_csv(input_file)
@@ -38,8 +56,6 @@ Qs = data['Q'].unique().tolist()
 Qs.sort()
 lowest_Q = Qs.pop(0)
 data_lowest_Q = data[data['Q'] == lowest_Q]
-if muts:
-    colors = ListedColormap(plt.cm.viridis(np.linspace(0, 1, len(muts))))
 
 if divergence_type == 'kl':
     kl_div_df = pd.DataFrame(columns=['Q', 'mutation', 'kl_divergence'])
@@ -62,7 +78,7 @@ if divergence_type == 'kl':
 
             label = f'{mut_label} mutations'
             bar_width = 1 / (len(muts) + 2)
-            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], kl_divergence, width=bar_width, label=label, color=colors(i))
+            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], kl_divergence, width=bar_width, label=label, color=mut_colors_dict[mut_label])
             # set the x ticks to be the Q values
             plt.xticks([x + 1 for x in range(len(Qs))], Qs)
         plt.title('KL divergence of Fixation Times')
@@ -89,7 +105,7 @@ if divergence_type == 'kl':
                 kl_divergence.append(divergence)
             label = f'{mut_label} mutations'
             bar_width = 1 / (len(muts) + 2)
-            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], kl_divergence, width=bar_width, label=label, color=colors(i))
+            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], kl_divergence, width=bar_width, label=label, color=mut_colors_dict[mut_label])
             # set the x ticks to be the Q values
             plt.xticks([x + 1 for x in range(len(Qs))], Qs)
             kl_div_df = pd.concat([kl_div_df, pd.DataFrame({'Q': Qs, 'mutation': [mut_label] * len(Qs), 'kl_divergence': kl_divergence})])
@@ -134,15 +150,18 @@ elif divergence_type == 'mean_percent_error':
                 data_lowest_mut = data_lowest_Q[f'mean_fixation_time_{mut}'].tolist()
                 data_lowest_mut = np.average(data_lowest_mut)
                 #print(data_lowest_mut[data_lowest_mut == 0])
-                divergence = (data_mut - data_lowest_mut)/data_lowest_mut
+                if data_mut == 0 or data_lowest_mut == 0:
+                    divergence = 0
+                else:  
+                    divergence = ((data_mut - data_lowest_mut)/data_lowest_mut) * 100
                 mean_divergence.append(divergence)
             label = f'{mut_label} mutations'
             bar_width = 1 / (len(muts) + 2)
-            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], mean_divergence, width=bar_width, label=label, color=colors(i))
+            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], mean_divergence, width=bar_width, label=label, color=mut_colors_dict[mut_label])
             # set the x ticks to be the Q values
             plt.xticks([x + 1 for x in range(len(Qs))], Qs)
             df_divergence = pd.concat([df_divergence, pd.DataFrame({'Q': Qs, 'mutation': [mut_label] * len(Qs), 'mean_percent_error': mean_divergence})])
-        plt.title('Mean Percent Error of Fixation Times')
+        plt.title('Mean Percent Error of Average Fixation Times')
         plt.xlabel('Q')
         plt.ylabel('Mean Percent Error')
         plt.legend()
@@ -159,15 +178,15 @@ elif divergence_type == 'mean_percent_error':
                 data_lowest_mut = data_lowest_Q[f'mean_sfs_{mut}'].tolist()
                 data_lowest_mut = np.average(data_lowest_mut)
                 #print(data_lowest_mut[data_lowest_mut == 0])
-                divergence = (data_mut - data_lowest_mut)/data_lowest_mut
+                divergence = ((data_mut - data_lowest_mut)/data_lowest_mut) * 100
                 mean_divergence.append(divergence)
             label = f'{mut_label} mutations'
             bar_width = 1 / (len(muts) + 2)
-            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], mean_divergence, width=bar_width, label=label, color=colors(i))
+            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], mean_divergence, width=bar_width, label=label, color=mut_colors_dict[mut_label])
             # set the x ticks to be the Q values
             plt.xticks([x + 1 for x in range(len(Qs))], Qs)
             df_divergence = pd.concat([df_divergence, pd.DataFrame({'Q': Qs, 'mutation': [mut_label] * len(Qs), 'mean_percent_error': mean_divergence})])
-        plt.title('Mean Percent Error of Site Frequency Spectra')
+        plt.title('Mean Percent Error of Average Allele Frequencies')
         plt.xlabel('Q')
         plt.ylabel('Mean Percent Error')
         plt.legend()
@@ -182,18 +201,18 @@ elif divergence_type == 'mean_percent_error':
             data_ld = np.average(data_ld)
             data_lowest_ld = data_lowest_Q['mean_ld'].tolist()
             data_lowest_ld = np.average(data_lowest_ld)
-            divergence = (data_ld - data_lowest_ld)/data_lowest_ld
+            divergence = ((data_ld - data_lowest_ld)/data_lowest_ld) * 100
             mean_divergence.append(divergence)
             df_divergence = pd.concat([df_divergence, pd.DataFrame({'Q': [Q], 'mean_percent_error': [divergence]})])
         plt.bar([x + 1 for x in range(len(Qs))], mean_divergence)
         plt.xticks([x + 1 for x in range(len(Qs))], Qs)
-        plt.title('Mean Percent Error of Linkage Disequilibrium')
+        plt.title('Mean Percent Error of Average Linkage Disequilibrium')
         plt.xlabel('Q')
         plt.ylabel('Mean Percent Error')
         plt.savefig(output_dir / 'graphs/mean_percent_error_ld.svg', bbox_inches='tight')
         df_divergence.to_csv(output_dir / 'summary_stats/mean_percent_error_ld.csv', index=False)
 
-    elif stat == 'fixation_probs':
+    elif stat == 'fixationprobs':
         mean_divergence = []
         for i, (mut, mut_label) in enumerate(zip(muts, mut_labels)):
             mean_divergence = []
@@ -205,20 +224,20 @@ elif divergence_type == 'mean_percent_error':
                 data_lowest_mut = data_lowest_Q[f'fixation_prob_{mut_no}'].tolist()
                 data_lowest_mut = np.average(data_lowest_mut)
                 #print(data_lowest_mut[data_lowest_mut == 0])
-                divergence = (data_mut - data_lowest_mut)/data_lowest_mut
+                divergence = ((data_mut - data_lowest_mut)/data_lowest_mut) * 100
                 mean_divergence.append(divergence)
                 df_divergence = pd.concat([df_divergence, pd.DataFrame({'Q': [Q], 'mutation': [mut_label], 'mean_percent_error': [divergence]})])
             label = f'{mut_label} mutations'
             bar_width = 1 / (len(muts) + 2)
-            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], mean_divergence, width=bar_width, label=label, color=colors(i))
+            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], mean_divergence, width=bar_width, label=label, color=mut_colors_dict[mut_label])
             # set the x ticks to be the Q values
             plt.xticks([x + 1 for x in range(len(Qs))], Qs)
-        plt.title('Mean Percent Error of Fixation Probabilities')
+        plt.title('Mean Percent Error of Average Fixation Probabilities')
         plt.xlabel('Q')
         plt.ylabel('Mean Percent Error')
         plt.legend()
-        plt.savefig(output_dir / 'graphs/mean_percent_error_fixation_probs.svg', bbox_inches='tight')
-        df_divergence.to_csv(output_dir / 'summary_stats/mean_percent_error_fixation_probs.csv', index=False)
+        plt.savefig(output_dir / 'graphs/mean_percent_error_fixationprobs.svg', bbox_inches='tight')
+        df_divergence.to_csv(output_dir / 'summary_stats/mean_percent_error_fixationprobs.csv', index=False)
 
 elif divergence_type == 'median_percent_error':
     df_divergence = pd.DataFrame(columns=['Q', 'mutation', 'median_percent_error'])
@@ -236,7 +255,7 @@ elif divergence_type == 'median_percent_error':
                 median_divergence.append(divergence)
             label = f'{mut_label} mutations'
             bar_width = 1 / (len(muts) + 2)
-            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], median_divergence, width=bar_width, label=label, color=colors(i))
+            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], median_divergence, width=bar_width, label=label, color=mut_colors_dict[mut_label])
             # set the x ticks to be the Q values
             plt.xticks([x + 1 for x in range(len(Qs))], Qs)
             df_divergence = pd.concat([df_divergence, pd.DataFrame({'Q': Qs, 'mutation': [mut_label] * len(Qs), 'median_percent_error': median_divergence})])
@@ -261,7 +280,7 @@ elif divergence_type == 'median_percent_error':
                 median_divergence.append(divergence)
             label = f'{mut_label} mutations'
             bar_width = 1 / (len(muts) + 2)
-            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], median_divergence, width=bar_width, label=label, color=colors(i))
+            plt.bar([x + 1 + i * bar_width for x in range(len(Qs))], median_divergence, width=bar_width, label=label, color=mut_colors_dict[mut_label])
             # set the x ticks to be the Q values
             plt.xticks([x + 1 for x in range(len(Qs))], Qs)
             df_divergence = pd.concat([df_divergence, pd.DataFrame({'Q': Qs, 'mutation': [mut_label] * len(Qs), 'median_percent_error': median_divergence})])
@@ -291,4 +310,23 @@ elif divergence_type == 'median_percent_error':
         plt.savefig(output_dir / 'graphs/median_percent_error_ld.svg', bbox_inches='tight')
         df_divergence.to_csv(output_dir / 'summary_stats/median_percent_error_ld.csv', index=False)
     
+
+elif divergence_type == 'average_rmse':
+    if stat == 'ld':
+        rmse = []
+        for Q in Qs:
+            data_Q = data[data['Q'] == Q]
+            data_ld = data_Q.filter(regex=r'ld_\d+').mean(axis = 0)
+            data_ld = data_ld.to_numpy()
+            data_lowest_ld = data_lowest_Q.filter(regex=r'ld_\d+').mean(axis = 0)
+            data_lowest_ld = data_lowest_ld.to_numpy()
+            # remove any columns if there are 
+            rmse.append(np.sqrt(np.mean((data_ld - data_lowest_ld)**2)))
+        plt.bar([x + 1 for x in range(len(Qs))], rmse)
+        plt.xticks([x + 1 for x in range(len(Qs))], Qs)
+        plt.title('Average RMSE of Linkage Disequilibrium')
+        plt.xlabel('Q')
+        plt.ylabel('Average RMSE')
+        plt.savefig(output_dir / 'graphs/average_rmse_ld.svg', bbox_inches='tight')
+        pd.DataFrame({'Q': Qs, 'average_rmse': rmse}).to_csv(output_dir / 'summary_stats/average_rmse_ld.csv', index=False)
 
